e2661bdf93fa3eb2dd0b45aaac3225b3a8d645c9
Diassisvilack <diassisdias@gmail.com>
2025-09-17 09:49:29 -0300

    Add files via upload

---
+++ DesafioTernario.php
@@ -0,0 +1,16 @@
+<?php
+
+$nota1 = $_GET['nota1'];
+$nota2 = $_GET['nota2'];
+$nota3 = $_GET['nota3'];
+ switch(nota){
+
+    case 1 : echo ("$media = ($nota1 + $nota2 + $nota3")) / 3;
+    case 2 : echo ("$situacao" = ($media >= 8.0) ? "Aluno Aprovado" : "Aluno Reprovado");
+    case 3 : echo ("$situacao" = ($media >= 8.0) ? "Aluno Aprovado" : "Aluno Reprovado");
+
+    default
+ }
+
+
+?>
