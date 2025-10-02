79cdc88134fdd20ef97673e251674060cc965771
Diassisvilack <diassisdias@gmail.com>
2025-10-02 09:48:25 -0300

    Add files via upload

--- ArrayAssociativo.php
+++ ArrayAssociativo.php
@@ -1,56 +1,45 @@
-<?php  
-$carros  =  [
-    'Uno'  =>  1991,
-    'Palio'  =>  2000,
-    'Gol'  =>  2025,
-    'Passat'  =>  1998,
-    'Parati'  =>  1999
+<?php 
+// An array to store car models and their corresponding years.
+$carros = [
+    'Uno'    => 1991,
+    'Palio'  => 2000,
+    'Gol'    => 2025,
+    'Passat' => 1998,
+    'Parati' => 1999
 ];
-if(!empty($_GET ['ano'] ) ) {
-    $ano = $_GET['ano'];
-    echo ("você selecionou o(s) modelo (s : ) ;
-  
 
+// Check if the form was submitted and if the 'anos' array is not empty.
+if (!empty($_GET['anos'])) {
+    $anos_selecionados = $_GET['anos'];
+
+    echo "Você selecionou os modelos com os seguintes anos:<br>";
+    
+    // Loop through the selected years and display them.
+    foreach ($anos_selecionados as $ano) {
+        // Use htmlspecialchars for security.
+        echo "- " . htmlspecialchars($ano) . "<br>";
+    }
 }
-
-}
-
-
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Seleção de Carros</title>
 </head>
 <body>
-    <form>
-        <?php foreach ($carros as $modelo => $ano ) {?>
-             <inpty type ="checkbox" name"ano[]" value="1991" <?php ?> echo $ano />
-             <label><?php  echo $modelo; ?></label>
-             <br>
+    <h2>Selecione os carros:</h2>
+    <form method="get">
+        <?php foreach ($carros as $modelo => $ano) { ?>
+            <input type="checkbox" name="anos[]" value="<?php echo htmlspecialchars($ano); ?>">
+            <label><?php echo htmlspecialchars($modelo) . ' (' . htmlspecialchars($ano) . ')'; ?></label>
+            <br>
         <?php } ?>
+        <br>
         <input type="submit" value="Enviar">
-        
-        <input type ="checkbox"/>
-        <label>Uno</label>
-        <br>
-        <input type ="checkbox"/>
-        <label>Palio</label>
-        <br>
-        <input type ="checkbox"/>
-        <label>Gol</label>
-        <br>
-        <input type ="checkbox"/>
-        <label>Passat</label>
-        <br>
-        <input type ="checkbox"/>
-        <label>Parati</label>
-        <br>
-        <input typer=" submit " value = "Enviar"/>
     </form>
-    
 </body>
 </html>
