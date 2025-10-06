582a1ffc738b492ce050423676ef266b224e6c21
Diassisvilack <diassisdias@gmail.com>
2025-10-06 08:44:06 -0300

    Add files via upload

---
+++ Informacaodoaluno.php
@@ -0,0 +1,16 @@
+<?php
+$nome ='joão';// isso e um nome
+$nomeSobrenome;
+$curso_turno_matricula = "adminitração Norturno 001";
+$compras_vendas_fornecedores;
+$idade = 75;
+$peso = '80pg';
+const PAIS = 'Brasil';
+$solteiro = true; 
+
+echo "Meu nome é $nome tenho $idade anos,
+ peso $peso,sou $solteiro moro no" .PAIS. "sou $solteiro";
+echo "meu curso $curso_turno_matricula"
+
+
+?>
