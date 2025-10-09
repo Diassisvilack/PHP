e418e45d239b1bea1ce9b29bbc00a47674e1c915
Diassisvilack <diassisdias@gmail.com>
2025-10-09 08:44:53 -0300

    Add files via upload

---
+++ selet.php
@@ -0,0 +1,26 @@
+<?php
+$Tecnologias = ['PHP', 'HTML','JAVASCRIPT','COBOL']
+echo ("você selecionou: ")
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post> 
+       <select name="selecao"> 
+       <option valeu="PHP">PHP</option>
+       <option valeu="HTML">HTML</option>
+       <option valeu="JAVASCRIPT">JAVASCRIPT</option>
+       <option valeu="COBOL">COBOL</option>
+       </select>
+       <input type="submit" value"Enviar"
+</form>
+    
+</body>
+</html>
+
